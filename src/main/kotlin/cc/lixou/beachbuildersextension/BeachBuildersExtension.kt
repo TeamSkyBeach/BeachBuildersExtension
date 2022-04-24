7dbd59c9653cc0276e6242dfd390fe354736e0fd
DasLixou <einlixou@gmail.com>
2022-04-24 18:00:27 +0200

    🪶 Using MenuItem in Extension

--- src/main/kotlin/cc/lixou/beachbuildersextension/BeachBuildersExtension.kt
+++ src/main/kotlin/cc/lixou/beachbuildersextension/BeachBuildersExtension.kt
@@ -1,15 +1,21 @@
 package cc.lixou.beachbuildersextension
 
+import cc.lixou.beachbuildersextension.items.MenuItem
+import net.minestom.server.event.player.PlayerSpawnEvent
 import net.minestom.server.extensions.Extension
 
 class BeachBuildersExtension : Extension() {
 
     override fun initialize() {
-        TODO("Not yet implemented")
+        eventNode.addListener(PlayerSpawnEvent::class.java) { event ->
+            val player = event.player
+            player.inventory.setItemStack(8, MenuItem.createItemStack())
+        }
+        println("[BeachBuilders] Initialized successfully")
     }
 
     override fun terminate() {
-        TODO("Not yet implemented")
+        println("[BeachBuilders] Terminated successfully")
     }
 
 }
